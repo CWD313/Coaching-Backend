e3d3f6112c0ac1b498b6956d6ec0bb63f175ee5e
Shubham Sharma <codewithshubh313@gmail.com>
2025-12-14 22:20:12 +0530

    Update exportController.js

--- src/controllers/exportController.js
+++ src/controllers/exportController.js
@@ -2,8 +2,8 @@ import ExcelJS from 'exceljs';
 import mongoose from 'mongoose';
 import { Student } from '../models/Student.js';
 import { Attendance } from '../models/Attendance.js';
-import TestMarks from '../models/TestMarks.js';
-import Test from '../models/Test.js';
+import { TestMarks }from '../models/TestMarks.js';
+import { Test }from '../models/Test.js';
 
 const { Types } = mongoose;
 

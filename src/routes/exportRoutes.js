00964948755bb6721f7ced7b89ffca3f4fc12145
Shubham Sharma <codewithshubh313@gmail.com>
2025-12-14 22:23:53 +0530

    Update exportRoutes.js

--- src/routes/exportRoutes.js
+++ src/routes/exportRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { authMiddleware } from '../middlewares/authMiddleware.js';
+import authMiddleware from '../middlewares/authMiddleware.js';
 import { loadTenantFlags } from '../middlewares/tenantFlags.js';
 import exportController from '../controllers/exportController.js';
 
